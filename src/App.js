62b1a0a5a596712aef19864b16ba68a098c718f8
Brett Seifried <seifriedbrett@gmail.com>
2021-12-16 14:37:51 -0800

    home page, to TeamList, to TeamDetail, NavLink added

--- src/App.js
+++ src/App.js
@@ -8,12 +8,24 @@ function App() {
   return (
     <main className="container">
       <BrowserRouter>
+        <header>
+          <NavLink to="/">Home</NavLink>
+        </header>
+        <header>
+          <NavLink to="/teams">Teams</NavLink>
+        </header>
+        <header>
+          <NavLink to="/players">Players</NavLink>
+        </header>
         <Switch>
           <Route exact path="/">
             <HomeDetail />
           </Route>
           <Route path="/teams">
             <TeamsList />
+          </Route>
+          <Route path="/teams:id">
+            <TeamsDetail />
           </Route>
         </Switch>
       </BrowserRouter>
--- src/Views/Teams/TeamsList.js
+++ src/Views/Teams/TeamsList.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import Team from '../../components/TeamFunction/Team';
 import { getTeams } from '../../services/teamRoute';
+import { Link } from 'react-router-dom';
 
 export default function TeamsList() {
   return (
     <ul className="team-list">
-      <li>TEAM NAME</li>
+      <li>
+        <Link to={'/teams/1'}> TEAM NAME</Link>
+      </li>
     </ul>
   );
 }
